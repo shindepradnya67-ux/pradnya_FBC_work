c8814a4a6c9e922fb754a38f2c147acd1e146354
Pradnya Shinde <shindepradnya67@gmail.com>
2026-08-10 18:40:01 +0530

    Delete Core Python/Assignments/Assignment11.py directory

--- Core Python/Assignments/Assignment11.py/Q3.py
+++
@@ -1,7 +0,0 @@
-## Python program to sort the list according to the second element in sublist.
-li=[[1,50],[2,20],[3,40],[4,10]]
-for i in range (1,len(li)):
-    for j in range(0,len(li)-i):
-        if li[j][1]>li[j+1][1]:
-            li[j],li[j+1]=li[j+1],li[j]
-print("Sorted List=",li)
